25ec7adc4ad78a9ed0683fcad3cfee614e0e24cd
Joe Yu <j56yu@uwaterloo.ca>
2022-11-10 14:33:40 -0500

    Flashlight now points at mouse cursor!

--- Assets/Scripts/flashlight_code.cs
+++ Assets/Scripts/flashlight_code.cs
@@ -1,14 +1,39 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Searcher;
 using UnityEngine;
 
 public class flashlight_code : MonoBehaviour
-{   //Great code, nice
+{
+    //Great code, nice
     // Start is called before the first frame update
     public GameObject ON;
     public GameObject OFF;
+    public GameObject flashlight;
+    public GameObject player;
+
+    [Header("Max angle")] public int horizontal = 45;
+    public int vertical = 15;
+
     private bool isON;
+
+    private Vector2 _center = new Vector2(Screen.width / 2, Screen.height / 2);
+
+    private Vector3 Angle(Vector3 mouse)
+    {
+        if (mouse.x < 0) mouse.x = 0;
+        if (mouse.y < 0) mouse.y = 0;
+        mouse.x -= _center.x;
+        mouse.y -= _center.y;
+        mouse.x /= _center.x;
+        mouse.y /= _center.y;
+        mouse.x *= Mathf.Deg2Rad * horizontal;
+        mouse.y *= Mathf.Deg2Rad * vertical;
+        Vector3 p = player.transform.eulerAngles;
+        return new Vector3(-Mathf.Rad2Deg * Mathf.Sin(mouse.y) + p.x,
+            Mathf.Rad2Deg * Mathf.Sin(mouse.x) + p.y, 0);
+    }
 
     void Start()
     {
@@ -21,16 +46,22 @@ public class flashlight_code : MonoBehaviour
     void Update()
     {
         Vector3 mouse = Input.mousePosition;
-        Debug.Log("Mouse.x" + mouse.x + "Mouse.y" +mouse.y);
-        if (Input.GetKeyDown(KeyCode.F)){
-            if(isON){
+        flashlight.transform.eulerAngles = Angle(mouse);
+        
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (isON)
+            {
                 ON.SetActive(false);
                 OFF.SetActive(true);
             }
-            if(!isON){
+
+            if (!isON)
+            {
                 ON.SetActive(true);
                 OFF.SetActive(false);
             }
+
             isON = !isON;
         }
     }
